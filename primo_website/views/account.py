be3e983b510d8f39292e0bbdf290f70641a04210
Antonio Serrano Muñoz <toni.sm91@gmail.com>
2019-04-30 12:07:17 -0400

    Make the page of accounts available for administrators

--- primo_website/views/account.py
+++ primo_website/views/account.py
@@ -9,9 +9,15 @@ from primo_website import login_manager
 mod = flask.Blueprint('account', __name__)
 
 @mod.route("/accounts", methods=["GET"])
+@flask_login.login_required
 def _accounts():
     """
+    Display the list of registered accounts.
+    This page is only available for administrators
     """
-    categories=["administrator", "physicist", "technologist", "nurse", "physician"]
-    accounts = controller.accounts()
-    return flask.render_template("accounts.html", accounts=accounts, categories=categories)
+    user = flask_login.current_user
+    if user.category == "administrator":
+        categories=["administrator", "physicist", "technologist", "nurse", "physician"]
+        accounts = controller.accounts()
+        return flask.render_template("accounts.html", accounts=accounts, categories=categories)
+    return flask.redirect(flask.url_for('general.index'))
